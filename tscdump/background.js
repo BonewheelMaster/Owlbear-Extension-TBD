cd76af21aee003a8713972456ca4d7ada5aa96e8
BonewheelMaster <bonewheelmaster@proton.me>
2026-08-30 01:58:23 -0400

    Add add and remove buttons, and a filter for the remove button
    The filter is to only appear on already-enabled tokens

--- tscdump/background.js
+++ tscdump/background.js
@@ -1,13 +1,40 @@
+//import OBR from "@owlbear-rodeo/sdk";
 import OBR from "@owlbear-rodeo/sdk";
-const message = "Hello World! V4";
-console.log(message);
-var counter = 0;
 // TODO change below to correct name
-const menuItem = {
-    id: "Owlbear-Extension-TBD/io.github.bonewheelmaster",
-    icons: [{ icon: "https://bonewheelmaster.github.io/Owlbear-Extension-TBD/icon.png", label: "Test" }],
-    onClick: (() => { counter++; OBR.notification.show(counter.toString()); }),
+const ID = "Owlbear-Extension-TBD/io.github.bonewheelmaster";
+const STATE_TAG = `${ID}/state`;
+function addToken(context) {
+    OBR.scene.items.updateItems(context.items, (items) => {
+        for (let item of items) {
+            item.metadata[STATE_TAG] = {
+                "enabled": true
+            };
+        }
+    });
+}
+function removeToken(context) {
+    OBR.scene.items.updateItems(context.items, (items) => {
+        for (let item of items) {
+            item.metadata[STATE_TAG] = {};
+        }
+    });
+}
+const menuAdd = {
+    id: ID,
+    icons: [{ icon: "https://bonewheelmaster.github.io/Owlbear-Extension-TBD/icon.png", label: "Instill thought" }],
+    onClick: (addToken),
+};
+const menuRemove = {
+    id: ID,
+    icons: [{ icon: "https://bonewheelmaster.github.io/Owlbear-Extension-TBD/icon.png",
+            label: "Uninstill thought",
+            filter: { every: [{ key: "layer", value: "CHARACTER" },
+                    { key: "metadata", value: { STATE_TAG: { "enabled": true } } }
+                ] }
+        }],
+    onClick: (removeToken),
 };
 OBR.onReady(() => {
-    OBR.contextMenu.create(menuItem);
+    OBR.contextMenu.create(menuAdd);
+    OBR.contextMenu.create(menuRemove);
 });
