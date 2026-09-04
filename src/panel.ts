08b47e889aa4677d7c3543c064cde091eb509ff9
BonewheelMaster <bonewheelmaster@proton.me>
2026-09-03 23:41:50 -0400

    Formatting

--- src/panel.ts
+++ src/panel.ts
@@ -1,5 +1,7 @@
 import OBR, { Item } from "@owlbear-rodeo/sdk";
 
+import * as state from "./state";
+
 const panelHTML = document.querySelector("#panel")
 if (panelHTML != null) { panelHTML.innerHTML = '<ul id="list"></ul>'; }
 
@@ -9,18 +11,18 @@ const STATE = `${ID}/state`
 const panel = (items : Item[]) => {
     const relevantItems = [];
     for (const item of items) {
-      const metadata = item.metadata[STATE];
-      if ( typeof(metadata) === "object" 
-           && metadata != null
-           && "enabled" in metadata
-           && metadata.enabled == true
+        const metadata = item.metadata[STATE];
+        if ( typeof(metadata) === "object" 
+            && metadata != null
+            && "enabled" in metadata
+            && metadata.enabled == true
         ) { relevantItems.push({ name: item.name }); }
     }
     const nodes = [];
     for (const item of relevantItems) {
-      const node = document.createElement("li");
-      node.innerHTML = `${item.name}`;
-      nodes.push(node);
+        const node = document.createElement("li");
+        node.innerHTML = `${item.name}`;
+        nodes.push(node);
     }
     const list = document.querySelector("#list");
     if (list != null) { list.replaceChildren(...nodes); }
