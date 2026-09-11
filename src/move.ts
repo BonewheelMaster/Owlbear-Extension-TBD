3dbcca0e066f88913665e41caf5e74dc147cbf1a
BonewheelMaster <bonewheelmaster@proton.me>
2026-09-11 13:09:44 -0400

    moveAll now moves the NPC's entire speed.

--- src/move.ts
+++ src/move.ts
@@ -14,14 +14,18 @@ export async function moveAll() {
     let newPositions : Record<string, Vector2> = {};
     for (let npc of npcs) { // This is done outside of the following because it is async.
         newPositions[npc.id] = await OBR.scene.grid.snapPosition(npc.position, 1, false, true);
-        //let expendedMovement = 0;
-        //while (expendedMovement < npc.meta.speed) {
-        //}
         const target = util.getTarget(items, npc.meta.target);
         if (target === null) { continue; }
-        const angle = Math.atan2(npc.position.y - target.position.y, npc.position.x - target.position.x);
-        newPositions[npc.id].x -= util.round(dpi * Math.cos(angle), dpi);
-        newPositions[npc.id].y -= util.round(dpi * Math.sin(angle), dpi);
+
+        // FIXME: if speed is not a multiple of 5 this will move more than allowed.
+        let expendedMovement = 0;
+        while (expendedMovement < npc.meta.speed) {
+            const angle = Math.atan2( newPositions[npc.id].y - target.position.y
+                                    , newPositions[npc.id].x - target.position.x);
+            newPositions[npc.id].x -= util.round(dpi * Math.cos(angle), dpi);
+            newPositions[npc.id].y -= util.round(dpi * Math.sin(angle), dpi);
+            expendedMovement += 5 // TODO hardcoded value for grid scale
+        }
     }
     OBR.scene.items.updateItems(npcs, (nn) => {
         for (let npc of nn) {
