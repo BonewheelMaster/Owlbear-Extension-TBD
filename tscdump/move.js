53300923236dbf0284f119abe1888342fc0fa5b8
BonewheelMaster <bonewheelmaster@proton.me>
2026-09-11 11:23:50 -0400

    moveAll now snaps each NPC to the nearest grid

--- tscdump/move.js
+++ tscdump/move.js
@@ -3,12 +3,16 @@ import * as util from "./util";
 export async function moveAll() {
     const items = await OBR.scene.items.getItems();
     const npcs = util.filterNPCs(items);
-    for (let npc of npcs) {
-        const pos1 = npc.position;
-        const target = util.getTarget(items, npc.meta.target);
-        if (target !== null) {
-            const dist = await OBR.scene.grid.getDistance(pos1, target.position);
-            console.log(`${util.getName(npc)}: ${dist}`);
+    OBR.scene.items.updateItems(npcs, (nn) => {
+        for (let npc of nn) {
+            //        const pos1 = npc.position;
+            //        const target = util.getTarget(items, npc.meta.target);
+            //        if (target !== null) { 
+            //            const dist = await OBR.scene.grid.getDistance(pos1, target.position)
+            //            console.log(`${util.getName(npc)}: ${dist}`);
+            //        }
+            OBR.scene.grid.snapPosition(npc.position, 1, false, true)
+                .then((newPos) => { npc.position = newPos; });
         }
-    }
+    });
 }
